fd3690a7d6fde8178b4f8e4b36c819d9102ec496
KV.ZZ <kevinzcharlie@gmail.com>
2024-12-08 16:08:35 -0800

    Make dark mode default

--- assets/js/skin-switch.js
+++ assets/js/skin-switch.js
@@ -1,10 +1,14 @@
 $(function() {
   $('#change-skin').on('click', function () {
     $("body").toggleClass("page-dark-mode");
-    localStorage.setItem('bj-dark-mode', $("body").hasClass("page-dark-mode"));
+    localStorage.setItem('bj-light-mode', !$("body").hasClass("page-dark-mode"));
     BeautifulJekyllJS.initNavbar();
   });
-  if (localStorage.getItem('bj-dark-mode') === 'true') {
+  if (localStorage.getItem('bj-light-mode') === 'true') {
     $('#change-skin').trigger('click');
   }
+});
+
+$().ready(function() {
+  $("body").css({ visibility:'visible' });
 });
